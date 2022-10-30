b640b210a1a87f7d909ecf608c1210db5b59e3c7
Lloyd Aipassa <lloyd.aipassa@gmail.com>
2022-10-30 18:46:58 +0100

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/Completed">Completed</router-link> |
-    <router-link to="/KnowledgeBase">KnowledgeBase</router-link> |
-    <router-link to="/Scheduled">Scheduled</router-link> |
-    <router-link to="/Settings">Settings</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-toolbar>
+     <h1>
+      test
+     </h1>
+    </v-toolbar>
+
+      <v-main>
+        <router-view/>
+      </v-main>
+      
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  data: () => ({
+    //
+  }),
 }
-
-nav a.router-link-exact-active {
-  color: #00AAA2;
-}
-</style>
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logoRealEstateCare.svg">
-    <h1><HelloWorld msg="Hello world!"/></h1> 
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
   name: 'HomeView',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
-
-<style>
-  .home{
-    background-color: #00AAA2;
-  }
-
-  h1{
-    color: white;
-  }
-</style>
